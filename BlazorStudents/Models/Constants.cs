f5e33c72b8f12dad0d309429597de7735960446b
WishYourBestCode <livelikenotomorrow88@gmail.com>
2025-02-22 10:15:18 -0800

    working

--- BlazorStudents/Models/Constants.cs
+++ BlazorStudents/Models/Constants.cs
@@ -3,7 +3,7 @@ namespace BlazorStudents.Models;
 public class Constants
 {
     //public const string BASE_URL = "https://studentfunctionmze.azurewebsites.net/";
-    //  public const string BASE_URL = "http://localhost:7071/";
-      public const string BASE_URL = "/";
+    // public const string BASE_URL = "http://localhost:7071/";
+    public const string BASE_URL = "/";
 }
 
