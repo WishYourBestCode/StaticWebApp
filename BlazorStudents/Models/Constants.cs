2bc7782fc557a8e04a4e85d5cf6d2d40ec2cb37d
WishYourBestCode <livelikenotomorrow88@gmail.com>
2025-02-22 00:07:24 -0800

    first commit

---
+++ BlazorStudents/Models/Constants.cs
@@ -0,0 +1,9 @@
+namespace BlazorStudents.Models;
+
+public class Constants
+{
+    //public const string BASE_URL = "https://studentfunctionmze.azurewebsites.net/";
+    //  public const string BASE_URL = "http://localhost:7071/";
+      public const string BASE_URL = "/";
+}
+
